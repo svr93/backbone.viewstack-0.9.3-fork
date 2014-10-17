ea0062b41fec21050fe4294699515b99ee6fdd8a
stephenhutchings <stephen@creativelicence.com.au>
2014-10-17 16:44:51 -0400

    Bump to 0.9.1

--- demo/public/js/vendor.js
+++ demo/public/js/vendor.js
@@ -138,6 +138,7 @@ var __hasProp = {}.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
 (function() {
+  "use strict";
   var isTouch;
   isTouch = "ontouchstart" in window;
   if (!Backbone) {
